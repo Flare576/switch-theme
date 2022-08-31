39d5cae922874db507f6c8209c67f53451d9f429
flare576 <flare576@gmail.com>
2022-08-31 18:18:21 -0500

    chore(output): copy features from dotfiles

--- bin/setupTerminalAppTheme.sh
+++ bin/setupTerminalAppTheme.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# To get the theme data, run
+# plist $HOME/Library/Preferences/com.apple.Terminal.plist
+# then find the key starting with `<key>THEMENAME</key>`
+# Exporting the terminal from within Terminal also works if you do this
+# https://apple.stackexchange.com/questions/99027/how-can-i-import-a-terminal-file-using-the-command-line
+
 THEMES="$1"
 
 # convenience method
@@ -20,3 +27,6 @@ for theme in $THEMES/**/*.terminal; do
   echo "Importing $themeName Terminal Theme"
   defaults write com.apple.Terminal "Window Settings" -dict-add "$themeName" "$bare"
 done
+
+divider="[41m[30m*[32m*[33m*[34m*[35m*[36m*[37m*[38m*[90m*[91m*[92m*[93m*[94m*[95m*[30m*[32m*[33m*[34m*[35m*[36m*[37m*[38m*[90m*[91m*[92m*[93m*[94m*[95m*[41m[30m*[32m*[33m*[34m*[35m*[0m"
+printf "\n${divider}\n[31mYOU WILL NEED TO RESTART TERMINAL[0m\nWe edited its plist\n${divider}\n"
